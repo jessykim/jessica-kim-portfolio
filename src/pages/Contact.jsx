7e7d030a444fecb6afda90e1590f9e9a5c7955d1
jessikim <jessica.kim17@gmail.com>
2022-10-14 22:43:38 -0500

    add contact information and create form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,25 @@
 function Contact() {
   return (
-    <h1>Contact</h1>
+    <>
+      <h1>Please feel free to contact me if you have any questions, comments or would like to collaborate!</h1>
+      <form action="">
+        <div>
+          <label htmlFor="">Name</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="">Email</label>
+          <input type="email" />
+        </div>
+        <div>
+          <label htmlFor="">Message</label>
+          <input type="text" />
+        </div>
+        <button>Send</button>
+      </form>
+      <a href="https://www.linkedin.com/in/jessicaykim/">LinkedIn</a>
+      <a href="https://github.com/jessykim">GitHub</a>
+    </>
   )
 }
 
