97b91ec9d9b4a263c3a46415e1e9cec9ca265084
jessikim <jessica.kim17@gmail.com>
2022-10-20 18:19:24 -0500

    create projects file in data folder and set up array of projects

---
+++ src/data/projects.js
@@ -0,0 +1,30 @@
+const projects = [
+  {
+    title: 'Tic-Tac-Turbo',
+    description: '',
+    image: '',
+    repositoryLink: 'https://github.com/jessykim/ttt-weekend',
+    deploymentLink: 'https://tictacturbo.netlify.app/'
+  },
+  {
+    title: '[star] WAR[s]',
+    description: '',
+    image: '',
+    repositoryLink: 'https://github.com/jessykim/star-WARs-card-game',
+    deploymentLink: 'https://star-war-s-card-game.netlify.app/'
+  },
+  {
+    title: 'Hustle ATL',
+    description: '',
+    image: '',
+    repositoryLink: 'https://github.com/jessykim/pick-up-bball',
+    deploymentLink: 'https://pick-up-bball.fly.dev/'
+  },
+  {
+    title: 'Giftology',
+    description: '',
+    image: '',
+    repositoryLink: '',
+    deploymentLink: ''
+  }
+]
