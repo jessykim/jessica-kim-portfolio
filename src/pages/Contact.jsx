04626901b7837cc5b3e4f9ee1ec109fcee5bc86c
jessikim <jessica.kim17@gmail.com>
2022-11-10 15:01:28 -0600

    style contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,7 @@
 import { React, useRef } from 'react'
+import styles from '../styles/Contact.module.css'
+import LinkedIn from '../assets/linkedin-icon.png'
+import GitHub from '../assets/github-icon.png'
 import emailjs from 'emailjs-com'
 
 function Contact() {
@@ -22,21 +25,26 @@ function Contact() {
 
   return (
     <>
-      <h1>Please feel free to contact me if you have any questions, comments or would like to collaborate!</h1>
-      <div>
-        <form ref={form} onSubmit={sendEmail}>
-          <label>Name</label>
-          <input type="text" name="user_name" required />
-          <label>Email</label>
-          <input type="email" name="user_email" required />
-          <label>Message</label>
-          <textarea name="message" required />
-          <input type="submit" value="Send" />
-        </form>
-      </div>
-      <div>
-        <a href="https://www.linkedin.com/in/jessicaykim/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-        <a href="https://github.com/jessykim" target="_blank" rel="noopener noreferrer">GitHub</a>
+      <div className={styles.contact}>
+        <div className={styles.contactContainer}>
+          <div className={styles.header}>
+            <h1>Contact</h1>
+            <p>Please feel free to contact me if you have any questions, comments or would like to collaborate! I hope to hear from you!</p>
+          </div>
+          <form ref={form} onSubmit={sendEmail}>
+            {/* <label>Name</label> */}
+            <input type="text" name="user_name" placeholder='Name' required />
+            {/* <label>Email</label> */}
+            <input type="email" name="user_email" placeholder='Email' required />
+            {/* <label>Message</label> */}
+            <textarea name="message" placeholder='Message' required />
+            <input type="submit" value="Send" id={styles.send} />
+          </form>
+          <div className={styles.contactLinks}>
+            <a href="https://www.linkedin.com/in/jessicaykim/" target="_blank" rel="noopener noreferrer"><img src={LinkedIn} alt="LinkedIn icon" /></a>
+            <a href="https://github.com/jessykim" target="_blank" rel="noopener noreferrer"><img src={GitHub} alt="GitHub icon" /></a>
+          </div>
+        </div>
       </div>
     </>
   )
