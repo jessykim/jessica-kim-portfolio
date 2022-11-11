1a4a9b8173cc21e80c3e03dec7ed2f8f064fe133
jessikim <jessica.kim17@gmail.com>
2022-11-11 00:42:09 -0600

    set up resume pdf page display with annotations and text layer

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,12 +1,15 @@
-import { React, useState }from 'react'
+import { React, useState } from 'react'
+import styles from '../styles/Resume.module.css'
 import resume from '../utilities/resume.pdf'
 import { Document, Page, pdfjs } from 'react-pdf/dist/esm/entry.webpack5'
+import 'react-pdf/dist/esm/Page/TextLayer.css'
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css'
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
 function Resume() {
   const [numPages, setNumPages] = useState(null)
   const [pageNumber, setPageNumber] = useState(1)
-  const [leftDisabled, setLeftDisabled] = useState(true);
+  const [leftDisabled, setLeftDisabled] = useState(true)
   const [rightDisabled, setRightDisabled] = useState(false)
 
   function onDocumentLoadSuccess({ numPages }) {
@@ -26,42 +29,44 @@ function Resume() {
   }
 
   return (
-    <div>
-      <h1>Resume</h1>
-      <div className='resume-pdf'>
-        <Document
-          file={resume}
-          onLoadSuccess={onDocumentLoadSuccess}
-          onLoadError={console.error}
-          style={{ width: '100vw', height: 'auto' }}
-          className='react-pdf__Page'
-        >
-          <Page pageNumber={pageNumber} />
-        </Document>
+    <>
+      <div className={styles.resume}>
+        <h1>Resume</h1>
+        <div className={styles.resumePdf}>
+          <Document
+            file={resume}
+            onLoadSuccess={onDocumentLoadSuccess}
+            onLoadError={console.error}
+            externalLinkTarget="_blank"
+            style={{ width: '100vw', height: 'auto' }}
+          >
+            <Page pageNumber={pageNumber} />
+          </Document>
+        </div>
+        <div className={styles.pageControls}>
+          <button 
+            disabled={leftDisabled}
+            type='button' 
+            onClick={changePage}
+          >
+            ‹
+          </button>
+          <span>{pageNumber} of {numPages}</span>
+          <button 
+            disabled={rightDisabled}
+            type='button' 
+            onClick={changePage}
+          >
+            ›
+          </button>
+        </div>
+        <div className={styles.download}>
+          <a href={resume} download='Jessica_Kim_Resume'>
+            <button>Download Resume</button>
+          </a>
+        </div>
       </div>
-      <div className='page-controls'>
-        <button 
-          disabled={leftDisabled}
-          type='button' 
-          onClick={changePage}
-        >
-          ‹
-        </button>
-        <span>{pageNumber} of {numPages}</span>
-        <button 
-          disabled={rightDisabled}
-          type='button' 
-          onClick={changePage}
-        >
-          ›
-        </button>
-      </div>
-      <div className='download-btn'>
-        <a href={resume} download='Jessica_Kim_Resume'>
-          <button>Download Resume</button>
-        </a>
-      </div>
-    </div>
+    </>
   )
 }
 
