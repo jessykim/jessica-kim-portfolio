da077798bb91746a58d52febebb6acb49848f34d
jessikim <jessica.kim17@gmail.com>
2022-11-12 22:06:43 -0600

    add information to project data file

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,53 +1,58 @@
 const projects = [
   {
     title: 'Tic-Tac-Turbo',
-    description: 'First browser game I created!',
+    description: 'Tic-Tac-Turbo was inspired by my pet rabbit, Turbo, who makes an appearance when a winner is declared. I wanted to add a personal touch to the game, which made it even more enjoyable to build!',
     image: 'https://i.imgur.com/20bdNb7.png',
     repositoryLink: 'https://github.com/jessykim/ttt-weekend',
     laptopImage: 'https://i.imgur.com/Id0GNk9.png',
     mobileImage_1: 'https://i.imgur.com/xuWhkhH.png',
     mobileImage_2: 'https://i.imgur.com/n2Ht94Y.png',
-    deploymentLink: 'https://tictacturbo.netlify.app/'
+    deploymentLink: 'https://tictacturbo.netlify.app/',
+    technologies: ['CSS', 'HTML', 'JavaScript']
   },
   {
     title: '[star] WAR[s]',
-    description: 'Unit 1 project',
+    description: '[star] WAR[s] is a themed browser game where users play War, a card game you may have heard of or played, against the computer.',
     image: 'https://i.imgur.com/g54wrRP.png',
     laptopImage: 'https://i.imgur.com/gJJsQls.png',
     mobileImage_1: 'https://i.imgur.com/xGT0S5z.png',
     mobileImage_2: 'https://i.imgur.com/XSkuX47.png',
     repositoryLink: 'https://github.com/jessykim/star-WARs-card-game',
-    deploymentLink: 'https://star-war-s-card-game.netlify.app/'
+    deploymentLink: 'https://star-war-s-card-game.netlify.app/',
+    technologies: ['CSS', 'HTML', 'JavaScript']
   },
   {
     title: 'Hustle ATL',
-    description: 'Unit 2 project using MEN stack',
+    description: 'Hustle ATL assists users with organizing pick-up basketball games and recording personal records/stats for games played. The inspiration behind this app came from speaking with a friend who plays basketball competitively and frequently hosts pick-up runs in Atlanta. This MEN stack application includes Google OAuth and was built using Express, a Node.js web application framework.',
     image: 'https://i.imgur.com/yJ199Gw.png',
     laptopImage: 'https://i.imgur.com/Bo2UJQf.png',
     mobileImage_1: 'https://i.imgur.com/bSxOXso.png',
     mobileImage_2: 'https://i.imgur.com/B93g7KJ.png',
     repositoryLink: 'https://github.com/jessykim/pick-up-bball',
-    deploymentLink: 'https://pick-up-bball.fly.dev/'
+    deploymentLink: 'https://pick-up-bball.fly.dev/',
+    technologies: ['Bootstrap', 'CSS', 'JavaScript', 'MongoDB', 'Node']
   },
   {
     title: 'Giftology',
-    description: 'Unit 3 project using MERN stack',
+    description: 'Giftology improves the gift-giving experience for users who may want some assistance on what their loved ones hope to receive for a special occassion or holiday. Users can view the wishlists of their friends as well as create their own. This MERN stack application includes JSON Web Token (JWT) and was built using Express, a Node.js web application framework.',
     image: 'https://i.imgur.com/svrY36B.png',
     laptopImage: 'https://i.imgur.com/axR13GB.png',
     mobileImage_1: 'https://i.imgur.com/iOyXHpi.png',
     mobileImage_2: 'https://i.imgur.com/AbJihnm.png',
     repositoryLink: 'https://github.com/creyes25/giftology-frontend',
-    deploymentLink: 'https://giftology-madeez.netlify.app/'
+    deploymentLink: 'https://giftology-madeez.netlify.app/',
+    technologies: ['Bootstrap', 'CSS', 'HTML', 'JavaScript', 'MongoDB', 'Node', 'React']
   },
   {
     title: 'Sweet Tooth',
-    description: 'Unit 4 project using Django, Python and PostgreSQL',
+    description: 'Sweet Tooth creates a space where dessert lovers can keep track of all their favorite desserts and recipes found online. Users can also create a list of locations where added desserts can be found.',
     image: 'https://i.imgur.com/kZZGTAE.png',
     laptopImage: 'https://i.imgur.com/8LV4LH0.png',
     mobileImage_1: 'https://i.imgur.com/qSkkkF6.png',
     mobileImage_2: 'https://i.imgur.com/4mKwE2f.png',
     repositoryLink: 'https://github.com/jessykim/dessert-collector-lab',
-    deploymentLink: 'TBD'
+    deploymentLink: 'TBD',
+    technologies: ['CSS', 'Django', 'HTML', 'PostgreSQL', 'Python']
   }
 ]
 
