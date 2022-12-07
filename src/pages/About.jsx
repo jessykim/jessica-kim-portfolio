8f19bcc641681f974d59d1e2864df609d46a5329
jessikim <jessica.kim17@gmail.com>
2022-12-07 15:42:33 -0600

    add photo credits to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -13,6 +13,9 @@ function About(props) {
             <p>Hello! I'm Jessica, a full stack software engineer who enjoys creating <span>innovative</span> and <span>effective</span> websites that considers the impact made on the user experience.</p>
             <p>When building projects, I use my background in chemistry to <span>think critically</span> and <span>problem solve</span> while incorporating skills from my experience in counseling/advising to approach needs for improvement with <span>empathy</span> and <span>strategic planning</span>.</p>
             <p>Producing meaningful work that serves a greater purpose is my <span>passion</span>.</p>
+            <p>Photo credits:
+              <a href="https://www.melshinphotography.com/" target="_blank" rel="noopener noreferrer"> Mel Shin</a>
+            </p>
           </div>
         </div>
         <div className={styles.joyList}>
