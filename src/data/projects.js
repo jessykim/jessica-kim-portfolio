27a4e4c74673cf4f186619d993a9480588ac3eec
jessikim <jessica.kim17@gmail.com>
2022-12-31 22:40:39 -0600

    add potluck planner to portfolio

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,36 +1,25 @@
 const projects = [
   {
-    title: 'Tic-Tac-Turbo',
-    description: 'Tic-Tac-Turbo was inspired by my pet rabbit, Turbo, who makes an appearance when a winner is declared. I wanted to add a personal touch to the game, which made it even more enjoyable to build!',
-    image: 'https://i.imgur.com/20bdNb7.png',
-    repositoryLink: 'https://github.com/jessykim/ttt-weekend',
-    laptopImage: 'https://i.imgur.com/Id0GNk9.png',
-    mobileImage_1: 'https://i.imgur.com/xuWhkhH.png',
-    mobileImage_2: 'https://i.imgur.com/n2Ht94Y.png',
-    deploymentLink: 'https://tictacturbo.netlify.app/',
-    technologies: ['CSS', 'HTML', 'JavaScript']
+    title: 'Potluck Planner',
+    description: 'Welcome to Potluck Planner! Here to help host your next potluck party! This side project was inspired by and built for my friend, Rina, who enjoys hosting potluck parties in her home and was interested in using another planning tool outside of Google spreadsheets. I thought this would be a good opportunity for me to build another full-stack application using the MERN stack and become more familiar with using React. This application also includes JSON Web Token (JWT) and was built using Express, a Node.js web application framework.',
+    image: '',
+    laptopImage: '',
+    mobileImage_1: '',
+    mobileImage_2: '',
+    repositoryLink: 'https://github.com/jessykim/potluck-planner',
+    deploymentLink: 'https://get-potluckin.netlify.app/',
+    technologies: ['Bootstrap', 'CSS', 'HTML', 'JavaScript', 'MongoDB', 'Node', 'React']
   },
   {
-    title: '[star] WAR[s]',
-    description: '[star] WAR[s] is a themed browser game where users play War, a card game you may have heard of or played, against the computer.',
-    image: 'https://i.imgur.com/g54wrRP.png',
-    laptopImage: 'https://i.imgur.com/gJJsQls.png',
-    mobileImage_1: 'https://i.imgur.com/xGT0S5z.png',
-    mobileImage_2: 'https://i.imgur.com/XSkuX47.png',
-    repositoryLink: 'https://github.com/jessykim/star-WARs-card-game',
-    deploymentLink: 'https://star-war-s-card-game.netlify.app/',
-    technologies: ['CSS', 'HTML', 'JavaScript']
-  },
-  {
-    title: 'Hustle ATL',
-    description: 'Hustle ATL assists users with organizing pick-up basketball games and recording personal records/stats for games played. The inspiration behind this app came from speaking with a friend who plays basketball competitively and frequently hosts pick-up runs in Atlanta. This MEN stack application includes Google OAuth and was built using Express, a Node.js web application framework.',
-    image: 'https://i.imgur.com/yJ199Gw.png',
-    laptopImage: 'https://i.imgur.com/Bo2UJQf.png',
-    mobileImage_1: 'https://i.imgur.com/bSxOXso.png',
-    mobileImage_2: 'https://i.imgur.com/B93g7KJ.png',
-    repositoryLink: 'https://github.com/jessykim/pick-up-bball',
-    deploymentLink: 'https://pick-up-bball.fly.dev/',
-    technologies: ['Bootstrap', 'CSS', 'JavaScript', 'MongoDB', 'Node']
+    title: 'Sweet Tooth',
+    description: 'Sweet Tooth creates a space where dessert lovers can keep track of all their favorite desserts and recipes found online. Users can also create a list of locations where added desserts can be found.',
+    image: 'https://i.imgur.com/kZZGTAE.png',
+    laptopImage: 'https://i.imgur.com/8LV4LH0.png',
+    mobileImage_1: 'https://i.imgur.com/qSkkkF6.png',
+    mobileImage_2: 'https://i.imgur.com/4mKwE2f.png',
+    repositoryLink: 'https://github.com/jessykim/dessert-collector-lab',
+    deploymentLink: 'https://sweet-tooth.up.railway.app/',
+    technologies: ['CSS', 'Django', 'HTML', 'PostgreSQL', 'Python']
   },
   {
     title: 'Giftology',
@@ -44,15 +33,37 @@ const projects = [
     technologies: ['Bootstrap', 'CSS', 'HTML', 'JavaScript', 'MongoDB', 'Node', 'React']
   },
   {
-    title: 'Sweet Tooth',
-    description: 'Sweet Tooth creates a space where dessert lovers can keep track of all their favorite desserts and recipes found online. Users can also create a list of locations where added desserts can be found.',
-    image: 'https://i.imgur.com/kZZGTAE.png',
-    laptopImage: 'https://i.imgur.com/8LV4LH0.png',
-    mobileImage_1: 'https://i.imgur.com/qSkkkF6.png',
-    mobileImage_2: 'https://i.imgur.com/4mKwE2f.png',
-    repositoryLink: 'https://github.com/jessykim/dessert-collector-lab',
-    deploymentLink: 'https://sweet-tooth.up.railway.app/',
-    technologies: ['CSS', 'Django', 'HTML', 'PostgreSQL', 'Python']
+    title: 'Hustle ATL',
+    description: 'Hustle ATL assists users with organizing pick-up basketball games and recording personal records/stats for games played. The inspiration behind this app came from speaking with a friend who plays basketball competitively and frequently hosts pick-up runs in Atlanta. This MEN stack application includes Google OAuth and was built using Express, a Node.js web application framework.',
+    image: 'https://i.imgur.com/yJ199Gw.png',
+    laptopImage: 'https://i.imgur.com/Bo2UJQf.png',
+    mobileImage_1: 'https://i.imgur.com/bSxOXso.png',
+    mobileImage_2: 'https://i.imgur.com/B93g7KJ.png',
+    repositoryLink: 'https://github.com/jessykim/pick-up-bball',
+    deploymentLink: 'https://pick-up-bball.fly.dev/',
+    technologies: ['Bootstrap', 'CSS', 'JavaScript', 'MongoDB', 'Node']
+  },
+  {
+    title: '[star] WAR[s]',
+    description: '[star] WAR[s] is a themed browser game where users play War, a card game you may have heard of or played, against the computer.',
+    image: 'https://i.imgur.com/g54wrRP.png',
+    laptopImage: 'https://i.imgur.com/gJJsQls.png',
+    mobileImage_1: 'https://i.imgur.com/xGT0S5z.png',
+    mobileImage_2: 'https://i.imgur.com/XSkuX47.png',
+    repositoryLink: 'https://github.com/jessykim/star-WARs-card-game',
+    deploymentLink: 'https://star-war-s-card-game.netlify.app/',
+    technologies: ['CSS', 'HTML', 'JavaScript']
+  },
+  {
+    title: 'Tic-Tac-Turbo',
+    description: 'Tic-Tac-Turbo was inspired by my pet rabbit, Turbo, who makes an appearance when a winner is declared. I wanted to add a personal touch to the game, which made it even more enjoyable to build!',
+    image: 'https://i.imgur.com/20bdNb7.png',
+    repositoryLink: 'https://github.com/jessykim/ttt-weekend',
+    laptopImage: 'https://i.imgur.com/Id0GNk9.png',
+    mobileImage_1: 'https://i.imgur.com/xuWhkhH.png',
+    mobileImage_2: 'https://i.imgur.com/n2Ht94Y.png',
+    deploymentLink: 'https://tictacturbo.netlify.app/',
+    technologies: ['CSS', 'HTML', 'JavaScript']
   }
 ]
 
